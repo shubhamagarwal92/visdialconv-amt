57d189b178891c37543496650770584d04d03eb4
Shubham Agarwal <shubhamagarwal92@gmail.com>
2020-04-09 01:10:31 +0100

    Qualification changes in launch_hits.py

--- launch_hits.py
+++ launch_hits.py
@@ -9,6 +9,12 @@ if __name__ == '__main__':
   parser.add_argument('--hit_properties_file', type=argparse.FileType('r'))
   parser.add_argument('--html_template')
   parser.add_argument('--input_json_file', type=argparse.FileType('r'))
+  parser.add_argument('--qual_questions', type=argparse.FileType('r'))
+  parser.add_argument('--qual_answers', type=argparse.FileType('r'))
+  parser.add_argument('--qual_test', action="store_true", help="If we want to have qualification test")
+  parser.add_argument('--qual_id', type=str, default="", help="Qualification id if already created")
+  parser.add_argument('--qual_name', type=str, default="", help="Name that we want to provide Qualification test")
+
   args = parser.parse_args()
 
   mtc = simpleamt.get_mturk_connection_from_args(args)
@@ -27,6 +33,34 @@ if __name__ == '__main__':
   if os.path.isfile(args.hit_ids_file):
     print('hit_ids_file already exists')
     sys.exit()
+
+  if args.qual_test:
+    if args.qual_id == "":
+      questions = args.qual_questions.read()
+      answers = args.qual_answers.read()
+
+      # A QualificationType's name must be unique among
+      # all of the QualificationTypes created by the same user.
+      qual_response = mtc.create_qualification_type(
+                    Name=args.qual_name,
+                    Keywords=hit_properties['Description'],
+                    Description=hit_properties['Keywords'],
+                    QualificationTypeStatus='Active',
+                    Test=questions,
+                    AnswerKey=answers,
+                    TestDurationInSeconds=300)
+
+      qual_reponse_id = qual_response['QualificationType']['QualificationTypeId']
+      print("New Qualification response id: ", qual_reponse_id )
+    else:
+      qual_reponse_id = args.qual_id
+      print("Using qualification response id: ", qual_reponse_id )
+
+    hit_properties['QualificationRequirements'].append({
+        'QualificationTypeId': qual_reponse_id,
+        'Comparator': 'EqualTo',
+        'IntegerValues': [100],
+    })
 
   with open(args.hit_ids_file, 'w') as hit_ids_file:
     for i, line in enumerate(args.input_json_file):
@@ -61,5 +95,3 @@ if __name__ == '__main__':
       hit_id = boto_hit['HIT']['HITId']
       hit_ids_file.write('%s\n' % hit_id)
       print('Launched HIT ID: %s, %d' % (hit_id, i + 1))
-
-
